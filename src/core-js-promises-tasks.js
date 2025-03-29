1faca0e4b07977e29a14a94572ffa96914b6af5d
Nikanora <olga.nikanorova@gmail.com>
2025-03-29 18:25:37 +0000

    feat: solve the third task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -56,8 +56,21 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  const newPromise = new Promise((resolve, reject) => {
+    const wrappPromise = promises.map((promise) =>
+      promise.catch(() => undefined)
+    );
+
+    Promise.race(wrappPromise).then((result) => {
+      if (result !== undefined) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    });
+  });
+  return newPromise;
 }
 
 /**
